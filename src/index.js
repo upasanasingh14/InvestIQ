931096f16f9dcf38778fcb4a190ae8e3a2e440ec
Upasana Singh <upasanas460@gmail.com>
2025-04-11 15:28:29 -0400

    added fontawesomefiles

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import HomePage from './landing_page/home/HomePage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+
     <HomePage/>
-   
+    
   </React.StrictMode>
 );
--- src/landing_page/home/HeroSection.js
+++ src/landing_page/home/HeroSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function HeroSection() {
     return ( 
-        <h1>HeroSection</h1>
+        <h1>HeroSection </h1>
      );
 }
 
